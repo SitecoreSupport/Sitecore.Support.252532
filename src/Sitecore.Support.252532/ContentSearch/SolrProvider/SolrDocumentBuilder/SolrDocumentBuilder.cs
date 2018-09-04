24f5cae16ca942abf02e8c63adfeb865050f8021
AndreyFilchenkov <AndreyFilchenkov@users.noreply.github.com>
2018-09-04 15:31:04 +0300

    Fix for #252532 issue

---
+++ src/Sitecore.Support.252532/ContentSearch/SolrProvider/SolrDocumentBuilder/SolrDocumentBuilder.cs
@@ -0,0 +1,20 @@
+﻿namespace Sitecore.Support.ContentSearch.SolrProvider
+{
+  using Sitecore.ContentSearch;
+  public class SolrDocumentBuilder : Sitecore.ContentSearch.SolrProvider.SolrDocumentBuilder
+  {
+    public SolrDocumentBuilder(IIndexable indexable, IProviderUpdateContext context)
+    : base(indexable, context) { }
+
+    protected override void AddField(IIndexableDataField field)
+    {
+
+      if (string.IsNullOrEmpty(field.Name) && string.IsNullOrEmpty(field.TypeKey))
+      {
+        return;
+      }
+
+      base.AddField(field);
+    }
+  }
+}
